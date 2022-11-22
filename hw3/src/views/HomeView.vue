e0a78fbb98a33f53c61f3ddd96097e1a1dea583f
Greete Kelli Aava <greete.kelli.aava@ut.ee>
2022-11-22 21:28:58 +0200

    Made button for resetting likes, no functionality yet

--- hw3/src/views/HomeView.vue
+++ hw3/src/views/HomeView.vue
@@ -11,6 +11,7 @@
       <aside>
       </aside>
   </div>
+  <b-button @click="">Reset likes</b-button>
 </template>
 
 <script>
@@ -104,4 +105,22 @@ aside {
 main {
     width: 60%;
 }
+
+b-button {
+    padding: 1rem 4rem;
+    text-align: center;
+    border-radius: 3px;
+    font-size: 10px;
+    text-transform: uppercase;
+    cursor: pointer;
+    background: #344cb7;
+    border: none;
+    color: #fff;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
+  b-button:hover {
+    background-color: rgba(82, 172, 113, 0.489);
+    transition: background-color 0.5s linear;
+  }
 </style>
