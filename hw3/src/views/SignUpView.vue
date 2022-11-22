3c8ff25c48fee07926d43a16a8bbb212180b6f02
meeri <meerimuru@gmail.com>
2022-11-22 22:33:05 +0200

    password validation

--- hw3/src/views/SignUpView.vue
+++ hw3/src/views/SignUpView.vue
@@ -1,32 +1,83 @@
 <template>
-    <div class="signup">
-      <div id="login">
-            <b>Welcome to postIt</b>
-            <p>Create an account</p>
-            <p>or</p>
-            <p>Please log in</p>
-            <form action="index.html">
+  <div class="signup">
+    <div id="login" v-cloak>
+
+      <h2>Sign up</h2>
+       <form action="index.html">
                 <input type="email" name="Email" placeholder="Email" required><br>
-                <input type="password" placeholder="Password" required><br>
-                <input type="submit" class="login" value="Log in"><br>
+                <div class="password">
+  <input :class='{valid:passwordValidation.valid}' :type="passwordVisible ? 'text' :  'password'" v-model="password" pattern="^(?=.*_)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$_!%*?&])[A-Za-z\d@$_!%*?&]{8,15}$" placeholder="Password" required></div>
+                <br><input type="submit" class="login" value="Sign up"><br>
             </form>
-            <p>Forget password</p>
-        </div>
-    </div>
+
+<transition name="req" appear>
+  <div v-if='passwordValidation.errors.length > 0 && !submitted' class='req'>
+    <h2>Requierments</h2>
+    <p v-for='error in passwordValidation.errors'>{{error}}</p>
+  </div>
+</transition>
+  </div>
+
+</div>
 </template>
 <style scoped>
-  .signup {
-    display: inline-block;
-    width: 40%;
-    align-items: center;
-    padding-top: 2%;
-  }
+.signup {
+  display: inline-block;
+  width: 40%;
+  align-items: center;
+  padding-top: 2%;
+}
 
-  #login {
-    background-color: rgb(152, 152, 152);
-  }
-  input{
-    box-sizing: border-box;
-    max-width: 100%;
-  }
+#login {
+  background-color: rgb(152, 152, 152);
+}
+input{
+  box-sizing: border-box;
+  max-width: 100%;
+}
+.req {
+	max-width:400px;
+	padding:1em;
+	margin: 1em 0;
+	font-size: .9em;
+	color:darken(#D4DEDF, 50%);
+}
 </style>
+
+<script>
+export default {
+	el:"#login",
+	data () {
+		return {
+			rules: [
+				{ message:'One lowercase letter required.', regex:/[a-z]+/ },
+				{ message:"One uppercase letter required.",  regex:/[A-Z]+/ },
+				{ message:"8 characters minimum, 15 max", regex:/.{8,15}/ },
+				{ message:"One number required.", regex:/[0-9]+/ },
+        { message:"_ required.", regex:/^(?=.*_)+/ },
+        { message:"First letter uppercase", regex:/^[A-Z]+[a-zA-Z]*$/}
+			],
+			password:'',
+			checkPassword:'',
+			submitted:false
+		}
+	},
+
+	computed: {
+		passwordValidation () {
+			let errors = []
+			for (let condition of this.rules) {
+				if (!condition.regex.test(this.password)) {
+					errors.push(condition.message)
+				}
+			}
+			if (errors.length === 0) {
+				return { valid:true, errors }
+			} else {
+				return { valid:false, errors }
+			}
+		}
+	}
+}
+	
+</script>
